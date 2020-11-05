2c51bbe505af64c9db361583c7fe327e837f11d9
mehalyna <firstmy555@gmail.com>
2020-11-05 19:53:40 +0200

    add project

---
+++ ShoppingSystem/Controllers/OrdersController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+using ShoppingSystem.Data;
+using ShoppingSystem.Models;
+
+namespace ShoppingSystem.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly ShoppingContext _context;
+
+        public OrdersController(ShoppingContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Orders
+        public async Task<IActionResult> Index()
+        {
+            var shoppingContext = _context.Orders.Include(o => o.Customer).Include(o => o.Supermarket);
+            return View(await shoppingContext.ToListAsync());
+        }
+
+        // GET: Orders/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+			var orderDetails = await _context.OrdersDetails.Where(o => o.OrderId == id).Include(p => p.Product).ToListAsync();
+            if (orderDetails == null)
+            {
+                return NotFound();
+            }
+
+            return View(orderDetails);
+        }
+
+        // GET: Orders/Create
+        public IActionResult Create()
+        {
+			ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "FirstName");
+			ViewData["SupermarketId"] = new SelectList(_context.Supermarkets, "Id", "Name");
+			return View();
+        }
+
+        // POST: Orders/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,CustomerId,SupermarketId,OrderDate")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(order);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Id", order.CustomerId);
+            ViewData["SupermarketId"] = new SelectList(_context.Supermarkets, "Id", "Id", order.SupermarketId);
+            return View(order);
+        }
+
+        // GET: Orders/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Id", order.CustomerId);
+            ViewData["SupermarketId"] = new SelectList(_context.Supermarkets, "Id", "Id", order.SupermarketId);
+            return View(order);
+        }
+
+        // POST: Orders/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,CustomerId,SupermarketId,OrderDate")] Order order)
+        {
+            if (id != order.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(order);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrderExists(order.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Id", order.CustomerId);
+            ViewData["SupermarketId"] = new SelectList(_context.Supermarkets, "Id", "Id", order.SupermarketId);
+            return View(order);
+        }
+
+        // GET: Orders/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders
+                .Include(o => o.Customer)
+                .Include(o => o.Supermarket)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        // POST: Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var order = await _context.Orders.FindAsync(id);
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OrderExists(int id)
+        {
+            return _context.Orders.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ShoppingSystem/Controllers/SupermarketsController.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShoppingSystem.Data;
+using ShoppingSystem.Models;
+using ShoppingSystem.Services;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingSystem.Controllers
+{
+    public class SupermarketsController : Controller
+    {
+        private readonly ShoppingContext context;
+
+        public SupermarketsController(ShoppingContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: Supermarkets
+        public async Task<IActionResult> Index(int? pageNumber, int pageSize = 3)
+        {
+            ViewBag.PageSize = pageSize;
+            var supermarkets = from s in context.Supermarkets select s;
+            return View(await PaginationService<Supermarket>.CreateAsync(supermarkets.AsNoTracking(), pageNumber ?? 1, pageSize));
+        }
+
+        // GET: Supermarkets/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var supermarket = await context.Supermarkets
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (supermarket == null)
+            {
+                return NotFound();
+            }
+
+            return View(supermarket);
+        }
+
+        // GET: Supermarkets/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Supermarkets/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Address")] Supermarket supermarket)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Add(supermarket);
+                await context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(supermarket);
+        }
+
+        // GET: Supermarkets/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var supermarket = await context.Supermarkets.FindAsync(id);
+            if (supermarket == null)
+            {
+                return NotFound();
+            }
+            return View(supermarket);
+        }
+
+        // POST: Supermarkets/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Address")] Supermarket supermarket)
+        {
+            if (id != supermarket.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    context.Update(supermarket);
+                    await context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SupermarketExists(supermarket.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(supermarket);
+        }
+
+        // GET: Supermarkets/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var supermarket = await context.Supermarkets
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (supermarket == null)
+            {
+                return NotFound();
+            }
+
+            return View(supermarket);
+        }
+
+        // POST: Supermarkets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var supermarket = await context.Supermarkets.FindAsync(id);
+            context.Supermarkets.Remove(supermarket);
+            await context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SupermarketExists(int id)
+        {
+            return context.Supermarkets.Any(e => e.Id == id);
+        }
+    }
+}
